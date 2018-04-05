984180c4124c034f3069b00fdc43685c3d12c0b7
wangxuan <demons_wx@163.com>
2018-04-06 00:00:54 +0800

    config eureka rabbitmq

--- config-client/src/main/java/online/wangxuan/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/online/wangxuan/configclient/ConfigClientApplication.java
@@ -2,7 +2,9 @@ package online.wangxuan.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
